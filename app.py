779ef449e16d680e4aab1df2eeb847bd0e5ce733
Matheus Remigio <matheus.remido@gmail.com>
2025-03-30 18:52:28 -0300

    create new purchase order

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -17,19 +17,35 @@ purchase_orders = [
 ]
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
     return "Hello World!"
 
-@app.route('/purchase_orders')
+@app.route('/purchase_orders', methods=['GET'])
 def get_purchase_orders():
     return jsonify(purchase_orders)
 
-@app.route('/purchase_orders/<int:id>')
+@app.route('/purchase_orders/<int:id>', methods=['GET'])
 def get_purchase_orders_by_id(id):
     for order in purchase_orders:
         if id == order['id']:
             return jsonify(order)
     return jsonify({'message': f'No order found for id {id}.'})
+
+@app.route('/purchase_orders', methods=['POST'] )
+def create_purchase_order():
+    request_data = request.get_json()
+    order = {
+        'id': request_data['id'],
+        'description': request_data['description'],
+        'items': []
+    }
+
+    purchase_orders.append(order)
+    
+    return jsonify(order)
+
+
+
 
 app.run(port=5000)
