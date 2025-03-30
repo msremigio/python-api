8162f00acbea54289f7816cd1bcdf8874c79a901
Matheus Remigio <matheus.remido@gmail.com>
2025-03-30 18:22:55 -0300

    Get order by id

--- app.py
+++ app.py
@@ -25,4 +25,11 @@ def home():
 def get_purchase_orders():
     return jsonify(purchase_orders)
 
+@app.route('/purchase_orders/<int:id>')
+def get_purchase_orders_by_id(id):
+    for order in purchase_orders:
+        if id == order['id']:
+            return jsonify(order)
+    return jsonify({'message': f'No order found for id {id}.'})
+
 app.run(port=5000)
