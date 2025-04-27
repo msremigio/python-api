f18a071b43a80590d20a1cb4ce89eebb36085a66
Matheus Remigio <matheus.remido@gmail.com>
2025-04-27 17:47:07 -0300

    Functionality changes in app.py and project structure definition through app_old.py

--- app_old.py
+++ app_old.py
@@ -1,72 +1,13 @@
-from flask import Flask, jsonify, request, abort
+from flask import Flask
+from flask_restful import Api
+from purchase_orders.resources import PurchaseOrders, PurchaseOrdersById
+from purchase_orders_items.resources import PurchaseOrdersItems
 
 app = Flask(__name__)
+api = Api(app)
 
-purchase_orders = [
-    {
-        'id': 1,
-        'description': 'Purchase order 1',
-        'items': [
-            {
-                'id': 1,
-                'description': 'First item from purchase order 1',
-                'price': 20.99,
-                'quantity': 2
-            }
-        ]
-    }
-]
+api.add_resource(PurchaseOrders, '/purchase_orders')
+api.add_resource(PurchaseOrdersById, '/purchase_orders/<int:id>')
+api.add_resource(PurchaseOrdersItems, '/purchase_orders/<int:id>/items')
 
-
-@app.route('/', methods=['GET'])
-def home():
-    return "Hello World!"
-
-@app.errorhandler(400)
-def bad_request(e):
-     return jsonify(error = str(e)), 400
-
-@app.route('/purchase_orders', methods=['GET'])
-def get_purchase_orders():
-    return jsonify(purchase_orders)
-
-@app.route('/purchase_orders/<int:id>', methods=['GET'])
-def get_purchase_orders_by_id(id):
-    for order in purchase_orders:
-        if id == order['id']:
-            return jsonify(order)
-    return jsonify({'message': f'No order found for id {id}.'})
-
-@app.route('/purchase_orders', methods=['POST'] )
-def post_purchase_order():
-    request_data = request.get_json()
-    order = {
-        'id': request_data['id'],
-        'description': request_data['description'],
-        'items': []
-    }
-
-    purchase_orders.append(order)
-    
-    return jsonify(order)
-
-@app.route('/purchase_order/<int:id>/items', methods=['GET'])
-def get_purchase_orders_items(id):
-    for order in purchase_orders:
-        if id == order['id']:
-            return jsonify(order['items'])
-    return jsonify({'message': f'No order found for id {id}.'})
-
-@app.route('/purchase_order/<int:id>/items', methods=['PUT'])
-def put_order_items(id):
-    request_data = request.get_json()
-    if type(request_data) != list:
-        abort(400, description="An order expects an array of item(s).")
-    for order in purchase_orders:
-        if id == order['id']:
-            _ = [order['items'].append(order_item) for order_item in request_data]
-            return jsonify(order)
-
-    return jsonify({'message': f'No order found for id {id}.'})
-
-app.run(port=5000)
+app.run(port=5000, debug=True)
