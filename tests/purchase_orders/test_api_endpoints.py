ade07ea2caef91a29eba6907e3f8fdae5632c554
Matheus Remigio <matheus.remido@gmail.com>
2025-05-03 18:20:56 -0300

    refactoring test cases

--- tests/purchase_orders/test_api_endpoints.py
+++ tests/purchase_orders/test_api_endpoints.py
@@ -20,22 +20,12 @@ def test_get_purchase_orders_by_id(test_client):
     assert response.json['id'] == 1
     assert response.json['description'] == 'Purchase order 1'
 
+def test_get_purchase_orders_by_id_not_found(test_client):
     response = test_client.get('/purchase_orders/999')
     
     assert response.json['message'] == 'No order found for id 999.'
 
 def test_post_purchase_orders(test_client):
-    body = {'id': 2, 'description': 'Purchase order 2'}
-
-    response = test_client.post(
-        'purchase_orders',
-        data=json.dumps(body),
-        content_type='application/json'
-    )
-
-    assert response.status_code == 400
-    assert response.json['error'] == '400 Bad Request: Use an array to POST one or many purchase orders.'
-
     body = [{'id': 2, 'description': 'Purchase order 2'}, {'id': 3, 'description': 'Purchase order 3'}]
 
     response = test_client.post(
@@ -50,11 +40,19 @@ def test_post_purchase_orders(test_client):
     assert response.json[1]['id'] == body[1]['id']
     assert response.json[1]['description'] == body[1]['description'] 
 
+def test_post_purchase_orders_error(test_client):
+    body = {'id': 2, 'description': 'Purchase order 2'}
+
+    response = test_client.post(
+        'purchase_orders',
+        data=json.dumps(body),
+        content_type='application/json'
+    )
+
+    assert response.status_code == 400
+    assert response.json['error'] == '400 Bad Request: Use an array to POST one or many purchase orders.'    
+
 def test_get_purchase_orders_items_by_id(test_client):
-    response = test_client.get('/purchase_orders/999/items')
-
-    assert response.json['message'] == 'No order found for id 999.'
-
     response = test_client.get('/purchase_orders/1/items')
 
     assert response.status_code == 200
@@ -63,28 +61,12 @@ def test_get_purchase_orders_items_by_id(test_client):
     assert response.json[0]['price'] == 20.99
     assert response.json[0]['quantity'] == 2
 
-def test_put_purchase_orders_items(test_client):
-    body = [{'id': 1, 'description': 'First item from purchase order 999', 'price': 99.99, 'quantity': 3}]
-
-    response = test_client.put(
-        'purchase_orders/999/items',
-        data=json.dumps(body),
-        content_type='application/json'
-    )
+def test_get_purchase_orders_items_by_id_not_found(test_client):
+    response = test_client.get('/purchase_orders/999/items')
 
     assert response.json['message'] == 'No order found for id 999.'
-    
-    body = {'id': 2, 'description': 'Second item from purchase order 1', 'price': 99.99, 'quantity': 3}
 
-    response = test_client.put(
-        'purchase_orders/1/items',
-        data=json.dumps(body),
-        content_type='application/json'
-    )
-
-    assert response.status_code == 400
-    assert response.json['error'] == '400 Bad Request: An order expects an array of item(s).'
-
+def test_put_purchase_orders_items_by_id(test_client): 
     body = [{'id': 2, 'description': 'Second item from purchase order 1', 'price': 99.99, 'quantity': 3}, {'id': 3, 'description': 'Third item from purchase order 1', 'price': 49.90, 'quantity': 7}]
 
     response = test_client.put(
@@ -102,3 +84,26 @@ def test_put_purchase_orders_items(test_client):
     assert response.json['items'][2]['description'] == body[1]['description'] 
     assert response.json['items'][2]['price'] == body[1]['price']
     assert response.json['items'][2]['quantity'] == body[1]['quantity']
+
+def test_put_purchase_orders_items_by_id_not_found(test_client):
+    body = [{'id': 1, 'description': 'First item from purchase order 999', 'price': 99.99, 'quantity': 3}]
+
+    response = test_client.put(
+        'purchase_orders/999/items',
+        data=json.dumps(body),
+        content_type='application/json'
+    )
+
+    assert response.json['message'] == 'No order found for id 999.' 
+
+def test_put_purchase_orders_items_by_id_error(test_client):
+    body = {'id': 2, 'description': 'Second item from purchase order 1', 'price': 99.99, 'quantity': 3}
+
+    response = test_client.put(
+        'purchase_orders/1/items',
+        data=json.dumps(body),
+        content_type='application/json'
+    )
+
+    assert response.status_code == 400
+    assert response.json['error'] == '400 Bad Request: An order expects an array of item(s).'
