34f057965875ba3962403c1c94150cc3072d123b
Matheus Remigio <matheus.remido@gmail.com>
2025-05-11 00:27:06 -0300

    jwt_required on api routes

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify, request
+from flask_jwt_extended import jwt_required
 from app.services.purchase_orders_service import PurchaseOrdersService
 from app.services.purchase_orders_items_service import PurchaseOrdersItemsService
 from app.services.users_service import UsersService
@@ -33,27 +34,33 @@ def user_auth():
     return UsersService.authenticate(request_data)
 
 @api_blueprint.route('/', methods=['GET'])
+@jwt_required()
 def home():
     return jsonify({'message': 'Welcome to the ***purchase_orders*** API homepage!'})
 
 @api_blueprint.route('/purchase_orders', methods=['GET'])
+@jwt_required()
 def get_purchase_orders():  
     return PurchaseOrdersService.get_all()
 
 @api_blueprint.route('/purchase_orders/<int:id>', methods=['GET'])
+@jwt_required()
 def get_purchase_orders_by_id(id):
     return PurchaseOrdersService.get_by_id(id)  
 
-@api_blueprint.route('/purchase_orders', methods=['POST'] )
+@api_blueprint.route('/purchase_orders', methods=['POST'])
+@jwt_required()
 def post_purchase_order():
     request_data = request.get_json()
     return PurchaseOrdersService.post_all(request_data)
 
 @api_blueprint.route('/purchase_orders/<int:id>/items', methods=['GET'])
+@jwt_required()
 def get_purchase_orders_items(id):
     return PurchaseOrdersItemsService.get_all_by_id(id)
 
 @api_blueprint.route('/purchase_orders/<int:id>/items', methods=['POST'])
+@jwt_required()
 def post_order_items(id):
     request_data = request.get_json()
     return PurchaseOrdersItemsService.post_all_by_id(id, request_data)
