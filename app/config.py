54dcad6dc408c49a3ab8314b2ce746ee539809c5
Matheus Remigio <matheus.remido@gmail.com>
2025-05-10 22:02:08 -0300

    jwt_manager configs

--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY=os.environ.get("JWT_SECRET")
 
 class TestingConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("TEST_DATABASE_URL")
--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from flask import Flask
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
 from app.api.routes import api_blueprint
 from app.extensions import db
 
@@ -13,6 +14,7 @@ def create_app(config_class="app.config.Config"):
 
     db.init_app(app)
     Migrate(app, db)
+    JWTManager(app)
 
     with app.app_context():
         db.create_all()
