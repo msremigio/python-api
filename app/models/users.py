81bef51dc192e4d2457269a3a80f38603c896bb0
Matheus Remigio <matheus.remido@gmail.com>
2025-05-10 23:24:03 -0300

    users table migration + model changes

--- app/models/users.py
+++ app/models/users.py
@@ -5,7 +5,7 @@ class UsersModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, index=True)
     email = db.Column(db.String(100), nullable=False, unique=True)
-    password = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.now(), nullable=False)
 
     def to_dict(self):
