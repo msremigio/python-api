183a1fa0ebb06b1aebd7035c4d068aca81cfadd0
Matheus Remigio <matheus.remido@gmail.com>
2025-05-11 23:24:17 -0300

    start refactoring tests for new /users routes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,21 @@
 import pytest
+import json
 from sqlalchemy.orm import scoped_session, sessionmaker
+from passlib.hash import pbkdf2_sha256
 from app.main import create_app
 from app.extensions import db
+from app.models.users import UsersModel
 from app.models.purchase_orders import PurchaseOrdersModel
 from app.models.purchase_orders_items import PurchaseOrdersItemsModel
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope='session')
 def test_client():
     client = create_app(config_class="app.config.TestingConfig")
     with client.test_client() as test_client:
         with client.app_context():
             yield test_client
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='module')
 def test_db_session():
     connection = db.engine.connect()
 
@@ -28,8 +31,13 @@ def test_db_session():
     test_session.remove()
     db.session = global_session
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='module')
 def seed_test_db(test_db_session):
+    test_user_exists = UsersModel.query.filter_by(email='perm_test_user@email.com').first()
+    if not test_user_exists:
+        insert_perm_test_user = UsersModel(email='perm_test_user@email.com', password=pbkdf2_sha256.hash('testingapp'))
+        test_db_session.add(insert_perm_test_user)
+        test_db_session.commit()
     insert_purchase_order = PurchaseOrdersModel(description="Purchase order for test")
     test_db_session.add(insert_purchase_order)
     test_db_session.commit()
@@ -38,8 +46,21 @@ def seed_test_db(test_db_session):
     test_db_session.commit()
     yield {'po': insert_purchase_order, 'poi': insert_purchase_order_item}
 
-@pytest.fixture(scope='function', autouse=True)
+@pytest.fixture(scope='module')
+def test_access_token(test_client):
+    body = {'email': 'perm_test_user@email.com', 'password': 'testingapp'}
+
+    response = test_client.post(
+        'users/authenticate',
+        data=json.dumps(body),
+        content_type='application/json'
+    )
+
+    yield response.json['access_token']
+
+@pytest.fixture(scope='module', autouse=True)
 def clear_test_db(test_db_session):
     test_db_session.query(PurchaseOrdersItemsModel).delete()
     test_db_session.query(PurchaseOrdersModel).delete()
+    test_db_session.query(UsersModel).filter(UsersModel.email != 'perm_test_user@email.com').delete()
     test_db_session.commit()
--- tests/test_api_endpoints.py
+++ tests/test_api_endpoints.py
@@ -1,39 +1,63 @@
 import json
 
-def test_get_homepage(test_client):
-    response = test_client.get('/')
+def test_post_user(test_client):
+    body = {'email': 'temp_test@email.com', 'password': 'temptestingapp'}
+
+    response = test_client.post(
+        'users',
+        data=json.dumps(body),
+        content_type='application/json'
+    )
+
+    assert response.status_code == 200
+    assert response.json['email'] == body['email']
+
+def test_post_user_auth(test_client, seed_test_db):
+    body = {'email': 'perm_test_user@email.com', 'password': 'testingapp'}
+
+    response = test_client.post(
+        'users/authenticate',
+        data=json.dumps(body),
+        content_type='application/json'
+    )
+
+    assert response.status_code == 200
+
+def test_get_homepage(test_client, test_access_token):
+    response = test_client.get('/', headers={'Authorization': f'Bearer {test_access_token}'})
 
     assert response.status_code == 200
     assert response.json['message'] == 'Welcome to the ***purchase_orders*** API homepage!'
 
-def test_get_purchase_orders(test_client, seed_test_db):
-    response = test_client.get('/purchase_orders')
+def test_get_purchase_orders(test_client, seed_test_db, test_access_token):
+    response = test_client.get('/purchase_orders', headers={'Authorization': f'Bearer {test_access_token}'})
 
     assert response.status_code == 200
     assert response.json[0]['id'] == seed_test_db['po'].id
     assert response.json[0]['description'] == seed_test_db['po'].description
     assert response.json[0]['status'] == seed_test_db['po'].status.value
 
-def test_get_purchase_orders_by_id(test_client, seed_test_db):
-    response = test_client.get(f'/purchase_orders/{seed_test_db['po'].id}')
+def test_get_purchase_orders_by_id(test_client, seed_test_db, test_access_token):
+    response = test_client.get(f'/purchase_orders/{seed_test_db['po'].id}', headers={'Authorization': f'Bearer {test_access_token}'})
 
     assert response.status_code == 200
     assert response.json['id'] == seed_test_db['po'].id
     assert response.json['description'] == seed_test_db['po'].description
     assert response.json['status'] == seed_test_db['po'].status.value
 
-def test_get_purchase_orders_by_id_not_found(test_client):
+def test_get_purchase_orders_by_id_not_found(test_client, test_access_token):
     id = 9999
-    response = test_client.get(f'/purchase_orders/{id}')
+    response = test_client.get(f'/purchase_orders/{id}', headers={'Authorization': f'Bearer {test_access_token}'})
     
     assert response.status_code == 404
     assert response.json['error'] == f'404 Not Found: No order found for id {id}.'
 
-def test_post_purchase_orders(test_client):
+def test_post_purchase_orders(test_client, test_access_token):
     body = [{'description': 'Test purchase order 2'}, {'description': 'Test purchase order 3'}]
 
     response = test_client.post(
         'purchase_orders',
+        headers={'Authorization': f'Bearer {test_access_token}'},
         data=json.dumps(body),
         content_type='application/json'
     )
@@ -42,11 +66,12 @@ def test_post_purchase_orders(test_client):
     assert response.json[0]['description'] == body[0]['description']
     assert response.json[1]['description'] == body[1]['description'] 
 
-def test_post_purchase_orders_error(test_client):
+def test_post_purchase_orders_error(test_client, test_access_token):
     body = {'description': 'Purchase order 2'}
 
     response = test_client.post(
         'purchase_orders',
+        headers={'Authorization': f'Bearer {test_access_token}'},
         data=json.dumps(body),
         content_type='application/json'
     )
@@ -54,8 +79,8 @@ def test_post_purchase_orders_error(test_client):
     assert response.status_code == 400
     assert response.json['error'] == '400 Bad Request: Use an array to POST one or many purchase orders.'    
 
-def test_get_purchase_orders_items_by_id(test_client, seed_test_db):
-    response = test_client.get(f'/purchase_orders/{seed_test_db['po'].id}/items')
+def test_get_purchase_orders_items_by_id(test_client, seed_test_db, test_access_token):
+    response = test_client.get(f'/purchase_orders/{seed_test_db['po'].id}/items', headers={'Authorization': f'Bearer {test_access_token}'})
 
     assert response.status_code == 200
     assert response.json[0]['id'] == seed_test_db['poi'].id
@@ -63,18 +88,19 @@ def test_get_purchase_orders_items_by_id(test_client, seed_test_db):
     assert response.json[0]['price'] == seed_test_db['poi'].price
     assert response.json[0]['quantity'] == seed_test_db['poi'].quantity
 
-def test_get_purchase_orders_items_by_id_not_found(test_client):
+def test_get_purchase_orders_items_by_id_not_found(test_client, test_access_token):
     id = 9999
-    response = test_client.get(f'/purchase_orders/{id}')
+    response = test_client.get(f'/purchase_orders/{id}', headers={'Authorization': f'Bearer {test_access_token}'})
     
     assert response.status_code == 404
     assert response.json['error'] == f'404 Not Found: No order found for id {id}.'
 
-def test_post_purchase_orders_items_by_id(test_client, seed_test_db): 
+def test_post_purchase_orders_items_by_id(test_client, seed_test_db, test_access_token): 
     body = [{'description': f'Second test item from purchase order {seed_test_db["po"].id}', 'price': 99.99, 'quantity': 3}, {'description': f'Third item from purchase order {seed_test_db["po"].id}', 'price': 49.90, 'quantity': 7}]
 
     response = test_client.post(
         f'purchase_orders/{seed_test_db["po"].id}/items',
+        headers={'Authorization': f'Bearer {test_access_token}'},
         data=json.dumps(body),
         content_type='application/json'
     )
@@ -87,12 +113,13 @@ def test_post_purchase_orders_items_by_id(test_client, seed_test_db):
     assert response.json[1]['price'] == body[1]['price']
     assert response.json[1]['quantity'] == body[1]['quantity']
 
-def test_post_purchase_orders_items_by_id_not_found(test_client):
+def test_post_purchase_orders_items_by_id_not_found(test_client, test_access_token):
     id = 9999
     body = [{'description': f'First item from purchase order {id}', 'price': 99.99, 'quantity': 3}]
 
     response = test_client.post(
         f'purchase_orders/{id}/items',
+        headers={'Authorization': f'Bearer {test_access_token}'},
         data=json.dumps(body),
         content_type='application/json'
     )
@@ -100,11 +127,12 @@ def test_post_purchase_orders_items_by_id_not_found(test_client):
     assert response.status_code == 404
     assert response.json['error'] == f'404 Not Found: No order found for id {id}.'
 
-def test_post_purchase_orders_items_by_id_error(test_client):
+def test_post_purchase_orders_items_by_id_error(test_client, test_access_token):
     body = {'description': 'Second item from purchase order 1', 'price': 99.99, 'quantity': 3}
 
     response = test_client.post(
         'purchase_orders/1/items',
+        headers={'Authorization': f'Bearer {test_access_token}'},
         data=json.dumps(body),
         content_type='application/json'
     )
